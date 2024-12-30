ada3f3031af5165b070b0f7bcf08c603338e8474
tracycute <21520255@gm.uit.edu.vn>
2024-12-30 20:45:03 +0700

    set up basic structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import News from './Components/News'
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='news-blog-app'>
+        <News />
+      </div>
+    </div>
+  )
+}
+
+export default App
