2094b2553599b409d415c1e78546f048af37bb6b
tracycute <21520255@gm.uit.edu.vn>
2024-12-30 22:39:45 +0700

    Creating Project Layout

---
+++ src/Components/Calendar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Calendar.css"
+
+const Calendar = () => {
+  return <div className='calendar'>Calendar</div>
+}
+
+export default Calendar
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Weather.css'
+
+const Weather = () => {
+  return <div className='weather'>Weather</div>
+}
+
+export default Weather
